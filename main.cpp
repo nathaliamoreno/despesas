52ec286d4cb81b7485efda42962a078e60feb50c
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-10 23:41:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,19 +6,16 @@ using namespace std;
 
 int main()
 {
-        Despesas despesas[3];
-        Despesas despesas[0] = Despesas("Boleto", 300);
-        Despesas despesas[1] = Despesas("Aluguel", 700);
-        Despesas despesas[2] = Despesas("Passagem", 500);
+        Despesas despesa1 = Despesas("Boleto", 300); //ATRIBUI VALORES
+        Despesas despesa2 = Despesas("Aluguel", 900);
 
-        ControleDeGastos gastosTotais = ControleDeGastos();
-        gastosTotais.setDespesa(despesas[]);
+        ControleDeGastos gastosTotais = ControleDeGastos(); //CRIA gastosTotais que recebe ControleDeGastos
 
+        gastosTotais.setDespesas(despesa1); //Passa valores para gastosTotais
 
         cout << "Valor total das despesas: R$ " << gastosTotais.CalculaTotalDespesa() << endl;
         cout << "Despesa com boleto: R$ " << gastosTotais.ExisteDespesaDoTipo("Boleto") << endl;
         cout << "Despesa com aluguel: R$ " << gastosTotais.ExisteDespesaDoTipo("Aluguel") << endl;
-        cout << "Despesa com aluguel: R$ " << gastosTotais.ExisteDespesaDoTipo("Passagem") << endl;
 
         cout <<"--------------------------------------" << endl;
 
